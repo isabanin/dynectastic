e3532ea0f6527ceb8ba9fb9415086ddbec7c0571
Ilya Sabanin <ilya.sabanin@gmail.com>
2010-10-29 10:47:44 +0800

    Experimental Job test added.

---
+++ test/test_job.rb
@@ -0,0 +1,32 @@
+require 'helper'
+
+class TestJob < Test::Unit::TestCase
+  
+  context "Job" do
+    setup do
+      @dynect = Dynectastic.session(DYNECT_CUST_NAME, DYNECT_USER_NAME, DYNECT_USER_PASS)
+      @zone = @dynect.zones.build(
+        :name    => "dynectastictests.com",
+        :contact => "ilya@wildbit.com",
+        :ttl     => 0
+      )
+      @zone.save
+    end
+    
+    teardown do
+      @zone.destroy
+    end
+    
+    should "be initialized when request is long running" do
+      start_ts = Time.now - 1296000
+      end_ts = Time.now + 1296000
+      resource = Dynectastic::Resource.new(@zone.session)
+      resource.post('/REST/QPSReport/', :body => {
+        :start_ts => start_ts.to_i,
+        :end_ts => end_ts.to_i 
+      })
+    end
+    
+  end
+  
+end
